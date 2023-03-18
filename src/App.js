69faf1be0c2bc32fc1490144f56e7be468c9950f
James Yang <jamesyang663@gmail.com>
2023-03-18 12:54:40 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
+import Landing from './pages/Landing';
+import Doctor from './pages/Doctor';
+import Patient from './pages/Patient';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={< />} />
+      <Route exact path="/" element={< Landing/>} />
+      <Route path="/doctor" element={< Doctor/>} />
+      <Route path="/patient" element={< Patient/>} />
     </Routes>
   );
 }
