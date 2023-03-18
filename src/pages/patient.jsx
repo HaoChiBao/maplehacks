e7d08dbaaf8b969890beee8540e0bf1f8170fe99
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-03-18 13:07:15 -0400

    added patient submit code

--- src/pages/patient.jsx
+++ src/pages/patient.jsx
@@ -1,8 +1,43 @@
-function Patient(){
-    return(
-        <div>
-            <h1>Patient</h1>
-        </div>
-    )
+function Patient() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the values, e.g. send them to a server
+    console.log("Name: ", name);
+    console.log("Password: ", password);
+
+    // Clear the inputs
+    setName("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Reason:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
-export default Patient
+
+export default Patient;
