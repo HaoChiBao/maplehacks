c81de19a6e05ec572fa474f5591f92a0997171a0
James Yang <jamesyang663@gmail.com>
2023-03-18 18:25:12 -0400

    big merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Landing from "./landing";
-import Doctor from "./Doctor/doctor";
-import Patient from "./Patient/Patient";
+
+import Patient from "./Patient/patient";
 import WaitingRoom from "./Patient/WaitingRoom";
-=======
-import {Route, Routes} from 'react-router-dom';
-import Landing from './landing';
-import Patient from './Patient/patient';
 
 import Doctor from './Doctor/doctor';
 import DoctorDashboard from './Doctor/dashboard';
->>>>>>> origin
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Landing />} />
       <Route path="/patient" element={<Patient />} />
-<<<<<<< HEAD
       <Route path="/waiting-room" element={<WaitingRoom />} />
-=======
       <Route path="/doctor" element={<Doctor />} />
       <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
->>>>>>> origin
     </Routes>
   );
 }
