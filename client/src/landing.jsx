738811586be93a47922503db6fb6b37b041aa0eb
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-03-18 13:39:21 -0400

    landing

--- client/src/landing.jsx
+++ client/src/landing.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
   const handleClick = () => {};
   return (
     <div>
       <h1>Landing</h1>
-      <button>Patient</button>
-      <button>Doctor</button>
+      <button>
+        <Link to={`/patient`}>Patient</Link>
+      </button>
+      <button>
+        {" "}
+        <Link to={`/doctor`}>Doctor</Link>
+      </button>
     </div>
   );
 };
