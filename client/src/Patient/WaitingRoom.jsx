0d980a8ef6d60f025f6677eb12c5a23a7aaf6402
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-03-19 03:09:04 -0400

    going to test james

--- client/src/Patient/WaitingRoom.jsx
+++ client/src/Patient/WaitingRoom.jsx
@@ -11,79 +11,90 @@ const WaitingRoom = () => {
   const [messages, setMessages] = useState([]);
   const [msgComponents, setMsgComponents] = useState();
   const [userQuery, setUserQuery] = useState("");
-  
+
   const gptQueryKey = (e) => {
-    if(e.key == "Enter"){
+    if (e.key == "Enter") {
       gptQuery();
     }
-  }
+  };
 
   const gptQuery = () => {
-    if(userQuery == ""){return;}
+    if (userQuery === "") {
+      return;
+    }
     // setMessages([...messages, {type: 'user', message: userQuery}]);
     // setMessages([...messages, {type: 'chatbot', message: "loading..."}]);
-    messages.push({type: 'user', message: userQuery});
-    messages.push({type: 'chatbot', message: "loading..."});
-    
+    messages.push({ type: "user", message: userQuery });
+    messages.push({ type: "chatbot", message: "loading..." });
+
     // console.log(userQuery);
-    console.log(messages)
+    console.log(messages);
     // setMsgComponents();
-    setMsgComponents(messages.map((message) => {
-      console.log(message)
-      return <ChatMessage type={message.type} message={message.message}/>
-    }))
-
-    console.log("Submitting")
-  
-    const url = "https://api.openai.com/v1/engines/text-davinci-003/completions";
-    const key = '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'
-    const bearer = 'Bearer ' + key
-    // console.log(bearer)
-  
-    let prompt = "Use the follow text as context of previous ChatGPT messages (note: instead of calling yourself ChatGPT, call yourself MedNow): \n\n"
-    for(let i = 0; i < messages.length-1; i++){
-      prompt += `${messages[i].type}: ${messages[i].message} \n\n`
-    }
-  
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': bearer,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            prompt: prompt,
-            temperature: 0.9,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-            stream: false,
-        })
-  
-    })
-    .then((response) => {return response.json()})
-    .then((data)=>{
-        // console.log(Object.keys(data))
-        console.log(data)
-        console.log(data['choices'][0].text)  
-
-        messages[messages.length-1] = {type: 'chatbot', message: data['choices'][0].text}
-        
+    setMsgComponents(
+      messages.map((message) => {
+        console.log(message);
+        return <ChatMessage type={message.type} message={message.message} />;
       })
-      .catch(error => {
-        console.log(error)
-        messages[messages.length-1] = {type: 'chatbot', message: error}
+    );
+
+    console.log("Submitting");
+
+    const url =
+      "https://api.openai.com/v1/engines/text-davinci-003/completions";
+    const key = "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";
+    const bearer = "Bearer " + key;
+    // console.log(bearer)
+
+    let prompt =
+      "Use the follow text as context of previous ChatGPT messages (note: instead of calling yourself ChatGPT, call yourself MedNow): \n\n";
+    for (let i = 0; i < messages.length - 1; i++) {
+      prompt += `${messages[i].type}: ${messages[i].message} \n\n`;
+    }
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: bearer,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        temperature: 0.9,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stream: false,
+      }),
     })
-    .finally(() => {
-      setMsgComponents(messages.map((message) => {
-        return <ChatMessage type={message.type} message={message.message}/>
-      }))
-    })
-    
-    
-  }
-   
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(Object.keys(data))
+        console.log(data);
+        console.log(data["choices"][0].text);
+
+        messages[messages.length - 1] = {
+          type: "chatbot",
+          message: data["choices"][0].text,
+        };
+      })
+      .catch((error) => {
+        console.log(error);
+        messages[messages.length - 1] = { type: "chatbot", message: error };
+      })
+      .finally(() => {
+        setMsgComponents(
+          messages.map((message) => {
+            return (
+              <ChatMessage type={message.type} message={message.message} />
+            );
+          })
+        );
+      });
+  };
+
   const [waitingRoomCount, setWaitingRoomCount] = useState(0);
   const [searchParams] = useSearchParams();
   const [waitingRoomQueue, setWaitingRoomQueue] = useState([]);
@@ -113,8 +124,8 @@ const WaitingRoom = () => {
   }, [searchParams]);
 
   return (
-    <div className = 'waitingRoom-page' onKeyDown={gptQueryKey}>
-      <div className = 'waiting-stat'>
+    <div className="waitingRoom-page" onKeyDown={gptQueryKey}>
+      <div className="waiting-stat">
         <h1>Waiting Room</h1>
 
         {userPosition >= 0 && (
@@ -135,12 +146,13 @@ const WaitingRoom = () => {
         </div>
 
         <div className="chatarea">
-
-          <div className="chatbox"> 
-            {msgComponents}
-          </div>
-          <div className = "queryBox">
-            <input type="text" placeholder="ask me something!" onChange={(e) => setUserQuery(e.target.value)}/>
+          <div className="chatbox">{msgComponents}</div>
+          <div className="queryBox">
+            <input
+              type="text"
+              placeholder="ask me something!"
+              onChange={(e) => setUserQuery(e.target.value)}
+            />
             <button onClick={gptQuery}>Send</button>
           </div>
         </div>
